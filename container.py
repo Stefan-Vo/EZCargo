d48ee1c42bd1cd217bf2f0900b1aa55dfc04c933
Stefan Vo <stef@ucr-secure-48-10-13-243-85.wnet.ucr.edu>
2024-11-19 19:07:55 -0800

    Base Classes

---
+++ container.py
@@ -0,0 +1,4 @@
+class Container:
+    def __init__(self, id, weight, hazardous=False):
+        self.id = id
+        self.weight = weight
---
+++ operators.py
@@ -0,0 +1,8 @@
+def load(ship, to_unload, to_load):
+    pass
+
+def unload(ship, to_unload, to_load):
+    pass
+
+def balance_ship(ship):
+    pass
